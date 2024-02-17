14a8de316f09fdf3222d3b26b251e79ad1ed2faa
futrime <futrime@outlook.com>
2024-02-17 10:37:21 +0800

    feat: use concatenation to build mirrored GitHub URLs

--- internal/network/github.go
+++ internal/network/github.go
@@ -2,17 +2,17 @@ package network
 
 import (
 	"fmt"
-	"net/url"
+	gourl "net/url"
 )
 
 // GenerateGitHubMirrorURL generates a GitHub mirror URL from a GitHub URL.
-func GenerateGitHubMirrorURL(url *url.URL, gitHubMirrorURL *url.URL) (*url.URL, error) {
+func GenerateGitHubMirrorURL(url *gourl.URL, gitHubMirrorURL *gourl.URL) (*gourl.URL, error) {
 	if !IsGitHubDirectDownloadURL(url) {
 		return nil, fmt.Errorf("not a GitHub URL: %v", url)
 	}
 
 	// Replace the host of the URL with the GitHub mirror URL.
-	mirroredURL, err := gitHubMirrorURL.Parse(url.Path)
+	mirroredURL, err := url.Parse(fmt.Sprintf("%v%v", gitHubMirrorURL, url.Path))
 	if err != nil {
 		return nil, fmt.Errorf("cannot parse GitHub mirror URL: %w", err)
 	}
@@ -21,6 +21,6 @@ func GenerateGitHubMirrorURL(url *url.URL, gitHubMirrorURL *url.URL) (*url.URL, 
 }
 
 // IsGitHubDirectDownloadURL checks if a URL is a GitHub URL that can be directly downloaded.
-func IsGitHubDirectDownloadURL(url *url.URL) bool {
+func IsGitHubDirectDownloadURL(url *gourl.URL) bool {
 	return url.Host == "github.com" && (url.Scheme == "http" || url.Scheme == "https")
 }
